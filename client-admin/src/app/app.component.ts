b0e7401f8ed469906cb6c2af554b8c9648e3e9ee
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2018-08-03 19:05:20 -0400

    Version du site dynamique.

--- client-admin/src/app/app.component.ts
+++ client-admin/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 import {MediaMatcher} from '@angular/cdk/layout';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import {MediaMatcher} from '@angular/cdk/layout';
 export class AppComponent implements OnDestroy {
 
   mobileQuery: MediaQueryList;
+  version: string;
   currentDate: Date;
 
   private readonly _mobileQueryListener: () => void;
@@ -17,6 +19,7 @@ export class AppComponent implements OnDestroy {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+    this.version = environment.version;
     this.currentDate = new Date();
   }
 
