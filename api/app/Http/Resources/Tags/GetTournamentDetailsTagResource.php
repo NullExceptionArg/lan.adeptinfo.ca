3c5db8d439af29cbfea6faa7a1661874193376f2
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2018-08-21 22:52:40 -0400

    lan.adeptinfo.ca-89 Consulter un tournoi. Correction du format de revoie de la requête.

--- api/app/Http/Resources/Tags/GetTournamentDetailsTagResource.php
+++ api/app/Http/Resources/Tags/GetTournamentDetailsTagResource.php
@@ -16,12 +16,12 @@ class GetTournamentDetailsTagResource extends Resource
     public function toArray($request)
     {
         return [
-            'tag_id' => $this->tag_id,
+            'tag_id' => intval($this->tag_id),
             'tag_name' => $this->tag_name,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'is_leader' => $this->is_leader == 1 ? true : false,
-            'reservation_id' => $this->reservation_id,
+            'reservation_id' => intval($this->reservation_id),
             'seat_id' => $this->seat_id
         ];
     }
