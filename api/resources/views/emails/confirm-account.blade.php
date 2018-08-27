71bc1ecb5b3bec0ce7094d11ab213ac0b10f40d4
Olivier Brassard <brassard.oli@gmail.com>
2018-08-26 21:30:56 -0400

    Courriel de confirmation d'adresse email

--- api/resources/views/emails/confirm-account.blade.php
+++ api/resources/views/emails/confirm-account.blade.php
@@ -1,2 +1,35 @@
-{% raw %}<strong>L'ADEPT vous souhaite la bienvenue! Pour confirmer votre code, veuillez accéder à l'url suivante: <a
-            href="{{env('BASE_URL')}}/api/user/confirm/{{$code}}">CONFIRMER</a></strong>!{% endraw %}
+<!-- {% raw %} -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="./confirmation-email.css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+    <img src="./assets/banner.png" id="header" class="banner">
+    <div id="content">
+        <h2>Bonjour <span class="green">{nom}</span>!</h2>
+        <p>
+            Merci d'avoir créé votre compte pour le LAN de l'ADEPT !
+        </p>
+        <p>
+            Il ne vous reste qu'une seule étape avant de pouvoir réserver votre place pour la prochaine édition du LAN!
+            Vous n'avez qu'a cliquer sur le bouton ci-dessous afin de confirmer votre adresse courriel. 
+        </p>
+        <a href="" class="btn-adept">Confirmer mon adresse</a>
+        <p>
+                Notez que vous ne recevrez aucun autre courriel de l'ADEPT Informatique tant que votre adresse ne sera pas confirmée.
+        </p>
+        <p class="small">
+            Pourquoi ais-je reçu ce courriel ? 
+            Quelqu'un a utilisé votre adresse courriel sur le site web lan.adeptinfo.ca.
+            Si ce n'est pas vous, ne vous inquietez pas. Vous ne recevrez aucun autre courriel.
+            Si vous avez des questions, n'hésitez pas à nous contacter par courriel à <a href="mailto:contact@adeptinfo.ca">contact@adeptinfo.ca</a>    
+        </p>
+
+    </div>
+    <img src="./assets/footer.png" id="footer" class="banner">
+</body>
+</html>
+<!-- {% endraw %} -->
