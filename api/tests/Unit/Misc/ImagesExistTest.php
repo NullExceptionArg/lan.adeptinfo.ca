b95e201a0154ce107403bfdaf3d53f059d72c325
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2018-08-28 07:35:09 -0400

    lan.adeptinfo.ca-104 Quitter une équipe Tests pour valider que le chemin vers les images existe. Remplacement d'une mauvaise image.

--- api/tests/Unit/Misc/ImagesExistTest.php
+++ api/tests/Unit/Misc/ImagesExistTest.php
@@ -7,10 +7,17 @@ use Tests\TestCase;
 
 class ImagesExistTest extends TestCase
 {
-    public function testBanner(): void
+    public function testBannerImageExist(): void
     {
         $client = new Client(['timeout' => 10.0]);
         $image = $client->get(env('BANNER_URL'))->getBody();
         $this->assertEquals(file_get_contents(base_path() . '/resources/views/emails/assets/banner.jpg'), $image->getContents());
+    }
+
+    public function testFooterImageExist(): void
+    {
+        $client = new Client(['timeout' => 10.0]);
+        $image = $client->get(env('FOOTER_URL'))->getBody();
+        $this->assertEquals(file_get_contents(base_path() . '/resources/views/emails/assets/footer.jpg'), $image->getContents());
     }
 }
