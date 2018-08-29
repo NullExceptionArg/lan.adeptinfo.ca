bb2ed85ff361f2de9b3302d0e6228580e0c55b42
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2018-08-28 20:30:27 -0400

    lan.adeptinfo.ca-104 Quitter une équipe Essaie pour fix travis.

--- api/tests/Unit/Misc/ImagesExistTest.php
+++ api/tests/Unit/Misc/ImagesExistTest.php
@@ -9,15 +9,17 @@ class ImagesExistTest extends TestCase
 {
     public function testBannerImageExist(): void
     {
-        $client = new Client(['base_uri' => 'https://raw.githubusercontent.com']);
-        $image = $client->request('GET', 'ADEPT-Informatique/lan.adeptinfo.ca/feature/email-confirmation/api/resources/views/emails/assets/banner.jpg')->getBody();
+        $url = parse_url(env('BANNER_URL'));
+        $client = new Client(['base_uri' => $url['scheme'] . '://' . $url['host']]);
+        $image = $client->request('GET', $url['path'])->getBody();
         $this->assertEquals(file_get_contents(base_path() . '/resources/views/emails/assets/banner.jpg'), $image->getContents());
     }
 
     public function testFooterImageExist(): void
     {
-        $client = new Client(['base_uri' => env('FOOTER_URL')]);
-        $image = $client->request('GET', '')->getBody();
+        $url = parse_url(env('FOOTER_URL'));
+        $client = new Client(['base_uri' => $url['scheme'] . '://' . $url['host']]);
+        $image = $client->request('GET', $url['path'])->getBody();
         $this->assertEquals(file_get_contents(base_path() . '/resources/views/emails/assets/footer.jpg'), $image->getContents());
     }
 }
