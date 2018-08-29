d6253b391dfeccb66974525128faada2121728c6
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2018-08-28 21:19:38 -0400

    lan.adeptinfo.ca-104 Quitter une équipe Essaie pour fix travis.

--- api/tests/Unit/Misc/ImagesExistTest.php
+++ api/tests/Unit/Misc/ImagesExistTest.php
@@ -9,16 +9,15 @@ class ImagesExistTest extends TestCase
 {
     public function testBannerImageExist(): void
     {
-        $client = new Client(['base_uri' => 'https://raw.githubusercontent.com']);
-        $image = $client->request('GET', 'ADEPT-Informatique/lan.adeptinfo.ca/feature/email-confirmation/api/resources/views/emails/assets/banner.jpg')->getBody();
+        $client = new Client();
+        $image = $client->get(env('BANNER_URL'))->getBody();
         $this->assertEquals(file_get_contents(base_path() . '/resources/views/emails/assets/banner.jpg'), $image->getContents());
     }
 
     public function testFooterImageExist(): void
     {
-        $url = parse_url(env('FOOTER_URL'));
-        $client = new Client(['base_uri' => $url['scheme'] . '://' . $url['host']]);
-        $image = $client->request('GET', ltrim($url['path'], '/'))->getBody();
+        $client = new Client();
+        $image = $client->get(env('FOOTER_URL'))->getBody();
         $this->assertEquals(file_get_contents(base_path() . '/resources/views/emails/assets/footer.jpg'), $image->getContents());
     }
 }
