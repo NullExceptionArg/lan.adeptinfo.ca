c174e3a36de46c03361e09739de41715a9acde05
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2019-03-27 18:28:03 -0400

    #115 Page Général - Détails Corrections liées au changement de structure.

--- clients/projects/admin/src/app/lan/create-lan/create-lan.component.ts
+++ clients/projects/admin/src/app/lan/create-lan/create-lan.component.ts
@@ -6,8 +6,9 @@ import {CreateLanSeatsComponent} from './seats/create-lan-seats.component';
 import {CreateLanCoordinatesComponent} from './coordinates/create-lan-coordinates.component';
 import {CreateLanRulesComponent} from './rules/create-lan-rules.component';
 import {CreateLanDescriptionComponent} from './description/create-lan-description.component';
-import {LanService} from 'core';
-import {Lan} from 'core';
+import {Errors, Lan, LanService} from 'core';
+import {SwalComponent} from '@sweetalert2/ngx-sweetalert2';
+import {Router} from '@angular/router';
 import {DateUtils} from '../../utils/DateUtils';
 
 @Component({
@@ -35,13 +36,26 @@ export class CreateLanComponent {
   // Formulaire de description
   @ViewChild(CreateLanDescriptionComponent) createLanDescriptionComponent: CreateLanDescriptionComponent;
 
+  // Les communications avec le serveur sont en cours, l'interface est donc désactivée pendant ce temps
+  isCreatingLan = false;
+
   // Surveille la largeur courante de l'écran de l'utilisateur
   mobileQuery: MediaQueryList;
+
+  // Erreurs qui peuvent être retournées par l'API
+  errors: Errors;
+
+  // Index courant du stepper
+  currentIndex = 0;
+
+  // Fenêtre qui confirme à l'utilisateur que le LAN a bien été créé
+  @ViewChild('createLanSuccessSwal') private createLanSuccessSwal: SwalComponent;
 
   constructor(
     private formBuilder: FormBuilder,
     private media: MediaMatcher,
-    private lanService: LanService
+    private lanService: LanService,
+    private router: Router
   ) {
     // Le changement de mobile à plein écran s'effectue lorsque l'écran fait 960 pixels de large
     this.mobileQuery = this.media.matchMedia('(min-width: 960px)');
@@ -101,12 +115,75 @@ export class CreateLanComponent {
 
     this.lanService.createLan(lan).subscribe(
       (data: Lan) => {
-        console.log(data);
+        this.errors = null;
+        this.isCreatingLan = false;
+        this.createLanSuccessSwal.show().then(() => this.router.navigateByUrl('/'));
       },
-      err => {
-        console.log(err);
+      (err: Errors) => {
+        this.errors = err;
+        this.isCreatingLan = false;
       }
     );
+  }
+
+  getErrorTitle(errorName: string): string {
+    switch (errorName) {
+      case 'name':
+        return 'Nom du LAN';
+      case 'lan_start':
+        return 'Date et heure de début';
+      case 'lan_end':
+        return 'Date et heure de fin';
+      case 'seat_reservation_start':
+        return 'Date et heure de réservation';
+      case 'tournament_reservation_start':
+        return 'Date et heure des tournois';
+      case 'event_key':
+        return 'Places';
+      case 'latitude':
+      case 'longitude':
+        return 'Coordonnées';
+      case 'places':
+        return 'Nombre de joueurs';
+      case 'price':
+        return 'Prix d\'entrée';
+      case 'rules':
+        return 'Règlements';
+      case 'description':
+        return 'Description';
+      default :
+        return 'Inconnu';
+    }
+  }
+
+  gotoError(error: string): void {
+    switch (error) {
+      case 'name':
+      case 'lan_start':
+      case 'lan_end':
+      case 'seat_reservation_start':
+      case 'tournament_reservation_start':
+      case 'places':
+      case 'price':
+        this.currentIndex = 0;
+        break;
+      case 'event_key':
+        this.currentIndex = 1;
+        break;
+      case 'latitude':
+      case 'longitude':
+        this.currentIndex = 2;
+        break;
+      case 'rules':
+        this.currentIndex = 3;
+        break;
+      case 'description':
+        this.currentIndex = 4;
+        break;
+      default :
+        this.currentIndex = 5;
+        break;
+    }
   }
 
   get rulesForm() {
