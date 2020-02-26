76e72a2c067794c7ed880a40e2c1a37496ac5855
Matai <mat4school@gmail.com>
2020-02-25 23:31:11 -0500

    CA FONCTIONNE

--- clients/projects/admin/src/app/app.module.ts
+++ clients/projects/admin/src/app/app.module.ts
@@ -1,13 +1,15 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { ApiService } from './../../../core/src/lib/services/api.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 import { core } from '@angular/compiler';
-import {FooterComponent} from './shared/layout/footer.component';
-import {SharedModule} from './shared/shared.module';
-import {AppRoutingModule} from './app-routing.module';
-import {AuthModule} from './auth/auth.module';
-import {LandingModule} from './landing/landing.module';
+import { FooterComponent } from './shared/layout/footer.component';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
+import { LandingModule } from './landing/landing.module';
+import { UserService, LanService } from 'projects/core/src/public_api';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,7 @@ import {LandingModule} from './landing/landing.module';
     LandingModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService, ApiService, LanService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
