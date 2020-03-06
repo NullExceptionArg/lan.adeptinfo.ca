ebdbb17f5c4b372bf7533b154cc9c23cec8b8fb2
Bourdages Félix <1659807@cegepmontpetit.ca>
2020-03-05 20:28:15 -0500

    Starting to set up system behind seat reservation

--- clients/projects/user/src/app/places/places.component.ts
+++ clients/projects/user/src/app/places/places.component.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'projects/core/src/public_api';
+import { Router } from '@angular/router';
+import { ApiService } from 'projects/core/src/lib/services/api.service';
+import { NgForm, Form, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-places',
   templateUrl: './places.component.html',
   styleUrls: ['./places.component.css']
 })
-export class PlacesComponent implements OnInit {
+export class PlacesComponent{
 
   config = {
     publicKey: "19aa9acc-c576-465e-bcbf-28738cb997a4",
-    event: "f1b13b46-abf7-469c-8663-7a0422384e8a",
+    event: "a2e58f40-980f-4b4f-9c1c-ec7098727e7a",
     fitTo: 'width',
-    style: { font: 'Roboto', border: 'max', padding: 'spacious' }
+    style: { font: 'Roboto', border: 'max', padding: 'spacious' },
+    features: { 
+      disabled: ['booths', 'tables']
+    },
+    maxSelectedObjects: 1,
+    selectedObjectsInputName: 'selectedSeat'
   }
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder,private userService: UserService, private router: Router, private apiService : ApiService) {
+    if(!this.userService.isAuthenticated)
+    {
+      router.navigate(['auth/login']);
+    }
+   }
 
-  ngOnInit() {
-  }
+   onSubmit(event: any){
+     return this.apiService.post('/seat/book/', event.target.selectedSeat.value).subscribe(
+       () => {this.router.navigate(["/Home"]);},
+       (error) =>{console.log(error);}
+     )
+   }
 
 }
