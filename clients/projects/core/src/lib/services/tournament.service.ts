0b9c5c5eaf6e471dcf4433574fd28d27a919651d
HandsomeRomanian <mat4school@gmail.com>
2020-03-10 12:45:01 -0400

    Creation service pour les tournois

---
+++ clients/projects/core/src/lib/services/tournament.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { ApiService } from "./api.service";
+
+@Injectable({
+  providedIn: "root"
+})
+export class TournamentamentService {
+  constructor(private apiService: ApiService) {}
+
+  createTournament(){
+
+  }
+
+  deleteTournament(){
+
+  }
+
+  tournamentByOrganizer(){
+
+  }
+
+  allTournament(){
+
+  }
+
+  updateTournament(){
+
+  }
+
+  detailsTournament(){
+
+  }
+
+  addOrganizer(){
+
+  }
+
+  removeOrganiser(){
+
+  }
+
+  
+}
